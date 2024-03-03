c216ebfd0020cae51480c486a526431cdda656d8
IBNODIL <ibnodil2010@gmail.com>
2024-03-03 21:23:23 +0500

    all in one page and do working

--- js/main.js
+++ js/main.js
@@ -4,30 +4,33 @@ let twoStar = document.getElementById('rating-btn-2')
 let threeStar = document.getElementById('rating-btn-3')
 let fourStar = document.getElementById('rating-btn-4')
 let fiveStar = document.getElementById('rating-btn-5')
+let ratingSection = document.getElementById('rating-section')
+let thankYouSection = document.getElementById('thank-you-section')
 let submitBtn = document.getElementById('submit')
 let rateOf = document.getElementById('rate-of')
 
 oneStar.addEventListener('click', () =>{
-    rateOf.textContent(rateTo)
     rateTo = 1
 })
 
 twoStar.addEventListener('click', () =>{
-    rateOf.textContent(rateTo)
     rateTo = 2
 })
 
 threeStar.addEventListener('click', () =>{
-    rateOf.textContent(rateTo)
     rateTo = 3
 })
 
 fourStar.addEventListener('click', () =>{
-    rateOf.textContent(rateTo)
     rateTo = 4
 })
 
 fiveStar.addEventListener('click', () =>{
-    rateOf.textContent(rateTo)
     rateTo = 5
+})
+
+submitBtn.addEventListener('click', () => {
+    rateOf.textContent = rateTo
+    ratingSection.classList.add('hidden')
+    thankYouSection.classList.remove('hidden')
 })
